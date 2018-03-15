5c541e04d18f91732a49f8ea61eea571e9a5601a
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 02:24:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,5 @@
 class Owner
+  attr_reader :species
   @@all = []
   @@count = 0
   def initialize(owner)
@@ -11,9 +12,12 @@ class Owner
   def self.count
     @@count
   end
-
   def self.reset_all
     @@all.clear
     @@count = 0
+  end
+
+  def species=(species)
+    @species = species
   end
 end
