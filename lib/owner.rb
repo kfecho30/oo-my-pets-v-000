101492cd05a6b711d01aa4d260e83e0eae318310
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 02:44:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -49,6 +49,6 @@ class Owner
     @pets.clear
   end
   def list_pets
-    "I have #{@pets[:fishes].length} fish"
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 end
